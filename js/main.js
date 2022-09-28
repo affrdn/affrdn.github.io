35d22ad3d7ccb6c5a0d0b6cf9588c7e0d9c20187
Afif Ramadhan <afifrmdn@gmail.com>
2022-09-29 02:58:29 +0700

    1.0

--- js/main.js
+++ js/main.js
@@ -1,33 +1,5 @@
 (function ($) {
     "use strict";
-
-    // Navbar on scrolling
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 200) {
-            $('.navbar').fadeIn('slow').css('display', 'flex');
-        } else {
-            $('.navbar').fadeOut('slow').css('display', 'none');
-        }
-    });
-
-
-    // Smooth scrolling on the navbar links
-    $(".navbar-nav a").on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            
-            $('html, body').animate({
-                scrollTop: $(this.hash).offset().top - 45
-            }, 1500, 'easeInOutExpo');
-            
-            if ($(this).parents('.navbar-nav').length) {
-                $('.navbar-nav .active').removeClass('active');
-                $(this).closest('a').addClass('active');
-            }
-        }
-    });
-
-
     // Typed Initiate
     if ($('.typed-text-output').length == 1) {
         var typed_strings = $('.typed-text').text();
@@ -40,78 +12,126 @@
         });
     }
 
+const connectButton = document.getElementById("connectButton");
+const walletID = document.getElementById("walletID");
 
-    // Modal Video
-    $(document).ready(function () {
-        var $videoSrc;
-        $('.btn-play').click(function () {
-            $videoSrc = $(this).data("src");
-        });
-        console.log($videoSrc);
+connectButton.addEventListener("click", () => {
+    // Start loader while connecting
+    const disconnectButton = document.getElementById("disconnectButton");
+    const copied = document.getElementById("copied");
 
-        $('#videoModal').on('shown.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-        })
+   if (typeof window.ethereum !== "undefined") {
+      ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then((accounts) => {
+            const account = accounts[0]
 
-        $('#videoModal').on('hide.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc);
-        })
-    });
+        //    connectButton.innerHTML = `Disconnect`;
+            walletID.innerHTML = `${account}`;  
+            
+            walletID.onclick = function(){
+                copied.classList.remove("d-none");
+                navigator.clipboard.writeText(`${account}`)
+            }
+        
+            // document.getElementById("copyBtn").onclick = function(){
+            //     navigator.clipboard.writeText(`${account}`)
+            // }
+
+            disconnectButton.classList.remove("d-none");
+            connectButton.classList.add("d-none");
+
+      }).catch((error) => {
+        // Handle error
+        console.log(error, error.code);
+
+        // Stop loader if error occured
+        // For example, when user cancelled request 
+        // and closed plugin
+        connectButton.classList.remove("connected");
+
+        // 4001 - The request was rejected by the user
+        // -32602 - The parameters were invalid
+        // -32603- Internal error
+      });
+   } else {
+      window.open("https://metamask.io/download/", "_blank");
+
+      // Show 'Reload page' warning to user
+      installAlert.classList.add("show");
+   }
+})
+    // $(document).ready(function () {
+    //     var $videoSrc;
+    //     $('.btn-play').click(function () {
+    //         $videoSrc = $(this).data("src");
+    //     });
+    //     console.log($videoSrc);
+
+    //     $('#videoModal').on('shown.bs.modal', function (e) {
+    //         $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+    //     })
+
+    //     $('#videoModal').on('hide.bs.modal', function (e) {
+    //         $("#video").attr('src', $videoSrc);
+    //     })
+    // });
 
 
     // Scroll to Bottom
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.scroll-to-bottom').fadeOut('slow');
-        } else {
-            $('.scroll-to-bottom').fadeIn('slow');
-        }
-    });
+    // $(window).scroll(function () {
+    //     if ($(this).scrollTop() > 100) {
+    //         $('.scroll-to-bottom').fadeOut('slow');
+    //     } else {
+    //         $('.scroll-to-bottom').fadeIn('slow');
+    //     }
+    // });
 
 
     // Skills
-    $('.skill').waypoint(function () {
-        $('.progress .progress-bar').each(function () {
-            $(this).css("width", $(this).attr("aria-valuenow") + '%');
-        });
-    }, {offset: '80%'});
+    // $('.skill').waypoint(function () {
+    //     $('.progress .progress-bar').each(function () {
+    //         $(this).css("width", $(this).attr("aria-valuenow") + '%');
+    //     });
+    // }, {offset: '80%'});
 
 
-    // Portfolio isotope and filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-    });
-    $('#portfolio-flters li').on('click', function () {
-        $("#portfolio-flters li").removeClass('active');
-        $(this).addClass('active');
+    // // Portfolio isotope and filter
+    // var portfolioIsotope = $('.portfolio-container').isotope({
+    //     itemSelector: '.portfolio-item',
+    //     layoutMode: 'fitRows'
+    // });
+    // $('#portfolio-flters li').on('click', function () {
+    //     $("#portfolio-flters li").removeClass('active');
+    //     $(this).addClass('active');
 
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
-    });
+    //     portfolioIsotope.isotope({filter: $(this).data('filter')});
+    // });
     
     
     // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 200) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+    // $(window).scroll(function () {
+    //     if ($(this).scrollTop() > 200) {
+    //         $('.back-to-top').fadeIn('slow');
+    //     } else {
+    //         $('.back-to-top').fadeOut('slow');
+    //     }
+    // });
+    // $('.back-to-top').click(function () {
+    //     $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+    //     return false;
+    // });
 
 
     // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        dots: true,
-        loop: true,
-        items: 1
-    });
+    // $(".testimonial-carousel").owlCarousel({
+    //     autoplay: true,
+    //     smartSpeed: 1500,
+    //     dots: true,
+    //     loop: true,
+    //     items: 1
+    // });
     
 })(jQuery);
+
 
